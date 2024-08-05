05bb37ec2f243a8ec6ac237076accf8cbe764cdd
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-05 09:21:45 -0600

    Fix typo

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe Flight, type: :model do
   describe "relationships" do
     it { should belong_to :airline }
-    it { should have_many ():passenger_flights) }
+    it { should have_many (:passenger_flights) }
     it { should have_many (:passengers) } # remove the through.(:passenger_flights) since it was throwing error.
   end
 end
