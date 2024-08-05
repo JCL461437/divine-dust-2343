37e0ca12fb64c0502795498edc16a6d359f4db53
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-05 10:39:52 -0600

    add redirection to the controller

--- app/controllers/passenger_flights_controller.rb
+++ app/controllers/passenger_flights_controller.rb
@@ -2,5 +2,6 @@ class PassengerFlightsController < ApplicationController
   def destroy
     pass_flight = PassengerFlight.find(flight: params[:flight_id], passenger: params[:id])
     pass_flight.destroy 
+    redirect_to flights_path
   end
 end
