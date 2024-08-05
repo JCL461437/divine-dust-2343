0769fcc7ed4f40f85411b6c634e813bc8f7f18fa
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-05 11:05:38 -0600

    Add first attempt at query for us 3

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,10 @@
 class Airline < ApplicationRecord
    has_many :flights
+
+   def adult_passengers
+      self.flights.joins(:passengers)
+         .select(:passengers)
+         .where("airline = self")
+         .where("passengers.age >= 18").distinct
+   end
 end
