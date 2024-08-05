357773e6cb9774479d4a7281a8937d325399fe72
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-05 09:18:06 -0600

    Added final associations

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 RSpec.describe Passenger, type: :model do
   describe "relationships" do
     it { should have_many :passenger_flights }
-    it { should belong_to (:passengers).through(:passenger_flights) }
+    it { should belong_to (:flights).through(:passenger_flights) }
   end
 end
